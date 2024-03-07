eea5311daebf7cf8387f62bf7da56f598c23b9e4
yusinh <yysh1020@gmail.com>
2024-03-07 15:03:44 +0900

    CastingExam

---
+++ day4/CastingExam1.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class CastingExam1 {
+    public static void main(String[] args) {
+        // 변수는 수정하지 않습니다.
+        int num1 = 10;
+        int num2 = 4;
+
+        System.out.println((double) num1 / num2); // 출력 : 2.5
+    }
+}
